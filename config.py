b8230aaf631bb9f5bf377af15795cc18ece29cee
Lover-Music <123320837+Lover-Music@users.noreply.github.com>
2025-01-18 16:59:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
-API_ID = 123456  # api id
-API_HASH = "ABC-DEF1234ghIkl-zyx57W2v1u123ew11"  # api hash
+API_ID = 22926746  # api id
+API_HASH = "ffd91926f59e55fb08b3e1a4f5b99b1d"  # api hash
 
-BOT_TOKEN = "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11"  # bot token
+BOT_TOKEN = "6223660988:AAFYthGatHBTspbuorp6aer2wv8KaBShZI4"  # bot token
 
 
 # REDIS
 HOST = "localhost"  # redis host uri
 PORT = 6379  # redis port
-PASSWORD = ""  # redis password
+PASSWORD = "q"  # redis password
 
 PRIVATE_CHAT_ID = -1001234567890  # CHAT WHERE YOU WANT TO STORE VIDEOS
 # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=;
-COOKIE = ""
+COOKIE = "q"
 ADMINS = [1317173146]
 
 
@@ -20,4 +20,4 @@ BOT_USERNAME = "teraboxdown_bot"
 # Force user to join this channel. (make sure you have promoted the bot on this chat.)
 FORCE_LINK = "@RoldexVerse"
 
-PUBLIC_EARN_API = ""  # https://publicearn.com/api
+PUBLIC_EARN_API = "q"  # https://publicearn.com/api
